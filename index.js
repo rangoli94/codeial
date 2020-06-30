8014668223f02e51f1d5ad38d87f51606bd2b7c4
rangoli <rangoli94@gmail.com>
2020-06-30 17:58:23 +0530

    added controller and router

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const port = 8000;
-const test = "test";
 const app = express();
+
+//use express router
+app.use('/',require('./routes/index'));
 
 app.listen(port,function(err) {
     if(err) {
